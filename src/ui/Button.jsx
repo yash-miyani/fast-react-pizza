96fda5fcfbf5b1ad228a6caba35d5a6e136f7948
yash-miyani <yash.miyani@0521.gmail.com>
2023-09-10 10:12:42 +0530

    Add Initial Files

---
+++ src/ui/Button.jsx
@@ -0,0 +1,35 @@
+import { Link } from 'react-router-dom';
+
+const Button = ({ children, disabled, to, type, onClick }) => {
+  const base =
+    'inline-block rounded-full text-sm bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 hover:bg-yellow-300 hover:transition-colors focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+  const styles = {
+    primary: base + ' px-4 py-3 md:px-6 md:py-4',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+
+    round: base + ' px-2.5 py-1 md:px-3.5 md:py-2 text-sm',
+    secondary:
+      'inline-block rounded-full font-semibold uppercase tracking-wide text-stone-400 hover:bg-stone-300 hover:text-stone-800 focus:text-stone-800 focus:bg-stone-300 hover:transition-colors focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed border-stone-300 border-2 px-4 py-2.5 md:px-6 md:py-3.5',
+  };
+
+  if (to)
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
+
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
+  return (
+    <button disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
